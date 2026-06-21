ec4c812c839b0ea3fc7004d5d112ca2f267c343b
Farid Zurita <zuritazurita19@gmail.com>
2026-06-21 03:29:48 -0600

    refactor: adaptacion para generar el archivo ejecutable

--- tutor-ia/electron.vite.config.js
+++ tutor-ia/electron.vite.config.js
@@ -1,9 +1,22 @@
+import { resolve } from 'path'
 import { defineConfig } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    main: {},
-    preload: {},
+    main: {
+        build: {
+            rollupOptions: {
+                input: resolve(__dirname, 'src/main/index.js')
+            }
+        }
+    },
+    preload: {
+        build: {
+            rollupOptions: {
+                input: resolve(__dirname, 'src/preload/index.js')
+            }
+        }
+    },
     renderer: {
         plugins: [vue()]
     }
